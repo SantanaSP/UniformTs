6e925469f9f2b7aafc2c13c2b15a628a73bd0434
Anonymous <no-reply@github.com>
2022-04-21 16:13:07 +0800

    Init

---
+++ packages/stream-web/index.ts
@@ -0,0 +1,4 @@
+const r = ReadableStream, rd = ReadableStreamDefaultReader, rdc = ReadableStreamDefaultController;
+export {r as ReadableStream, rd as ReadableStreamDefaultReader, rdc as ReadableStreamDefaultController};
+const w = WritableStream, wd = WritableStreamDefaultWriter, wdc = WritableStreamDefaultController;
+export {w as WritableStream, wd as WritableStreamDefaultWriter, wdc as WritableStreamDefaultController};
---
+++ tests/nodejs-env/src/web-api/stream.spec.ts
@@ -0,0 +1,37 @@
+import {
+  ReadableStream,
+  WritableStream,
+} from "@sparklink/uniform-ts-stream-web";
+import { setInterval as every } from "node:timers/promises";
+
+import { performance } from "node:perf_hooks";
+
+describe("stream-web module", function () {
+  it("should function well under nodejs.", async function () {
+    const SECOND = 100;
+
+    const stream = new ReadableStream({
+      async start(controller) {
+        let counter = 0;
+        for await (const _ of every(SECOND)) {
+          controller.enqueue(performance.now());
+          counter++;
+          if (counter >= 3) controller.close();
+        }
+      },
+    });
+
+    await new Promise<void>((resolve, reject) => {
+      stream.pipeTo(
+        new WritableStream({
+          write(chunk) {
+            console.log(chunk);
+          },
+          close() {
+            resolve();
+          },
+        })
+      );
+    });
+  });
+});
